6f43a802e1a26526ce8ae079a6d824ad282cf506
Evgeniy <svorovali@mail.ru>
2023-12-08 11:43:03 +0300

    Create file task2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,48 @@
 package org.example;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+
 public class Main {
+
+
+    private static final List<String> validPaths = List.of("/index.html", "/spring.svg",
+            "/spring.png", "/resources.html", "/styles.css", "/app.js",
+            "/links.html", "/forms.html", "/events.html", "/events.js");
+
     public static void main(String[] args) {
-        new Server().start(9999);
+        var server = new Server();
+
+        for (String validPath : validPaths) {
+            server.addHandler("GET", validPath, (request, responseStream) -> {
+                try {
+                    final Path filePath = Path.of(".", "public", request.getRequestLine().getPath());
+                    byte[] content = Files.readAllBytes(filePath);
+                    ResponseUtils.responseOk(filePath, content, responseStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        server.addHandler("GET", "/classic.html", (request, responseStream) -> {
+            try {
+                final Path filePath = Path.of(".", "public", request.getRequestLine().getPath());
+                String fileContent = Files.readString(filePath);
+                byte[] content = fileContent
+                        .replace("{time}", LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")))
+                        .getBytes();
+                ResponseUtils.responseOk(filePath, content, responseStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        server.listen(9999);
     }
 }
--- src/main/java/org/example/ResponseUtils.java
+++ src/main/java/org/example/ResponseUtils.java
@@ -8,19 +8,21 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class ResponseUtils {
-    public static void responseOk(BufferedOutputStream out, Path filePath) throws IOException {
-        String template = Files.readString(filePath);
-        String content = template.replace("{time}", LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
-        byte[] contentBytes = content.getBytes();
-
-        String response = "HTTP/1.1 200 OK\r\n" +
-                "Content-Type: text/html\r\n" +
-                "Content-Length: " + contentBytes.length + "\r\n" +
-                "Connection: close\r\n" +
-                "\r\n";
-        out.write(response.getBytes());
-        out.write(contentBytes);
-        out.flush();
+    public static void responseOk(Path filePath, byte[] content, BufferedOutputStream out) throws IOException {
+        try {
+            String mimeType = Files.probeContentType(filePath);
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + content.length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            out.write(content);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void badRequest(BufferedOutputStream out) throws IOException {
@@ -30,27 +32,6 @@ public class ResponseUtils {
                         "Connection: close\r\n" +
                         "\r\n"
         ).getBytes());
-        out.flush();
-    }
-
-    public static void classicHtml(BufferedOutputStream out, Path filePath, String mimeType) throws IOException {
-        long length = Files.size(filePath);
-
-        String response = "HTTP/1.1 200 OK\r\n" +
-                "Content-Type: " + mimeType + "\r\n" +
-                "Content-Length: " + length + "\r\n" +
-                "Connection: close\r\n" +
-                "\r\n";
-        out.write(response.getBytes());
-        Files.copy(filePath, out);
-        out.flush();
-    }
-    public static void sendBadRequestResponse(BufferedOutputStream out) throws IOException {
-        String response = "HTTP/1.1 400 Bad Request\r\n" +
-                "Content-Length: 0\r\n" +
-                "Connection: close\r\n" +
-                "\r\n";
-        out.write(response.getBytes());
         out.flush();
     }
 }
